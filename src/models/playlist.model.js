635ae662cfaf8435ac6939d63fb47179fd05f001
Piyush <piyushlikeyou@gmail.com>
2024-09-06 15:13:26 +0530

    playlist model

---
+++ src/models/playlist.model.js
@@ -0,0 +1,24 @@
+import mongoose, {Schema} from "mongoose";
+
+const playlistSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        videos: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Video"
+            }
+        ],
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }
+    },{timestamps: true}
+)
